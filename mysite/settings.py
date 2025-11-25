f86a98605162fee1c801b9ee1cd33188ea785644
summer193223 <sypsummer2009@gmail.com>
2025-11-25 22:48:18 +0900

    add gunicorn

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-(%!n5&%p*ih^64dx4*3-3q1z4l4zs-tw45@mkrm#&d!n$!jree
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
